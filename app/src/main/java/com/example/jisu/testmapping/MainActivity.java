e090e53b91e0bf9459e7b5ccb24cd5bf24cdddd6
tjwhdwn <tjwhdwn@naver.com>
2019-12-13 15:29:44 +0900

    GPS에 따른 범위를 세팅 맵 주석처리

--- app/src/main/java/com/example/jisu/testmapping/MainActivity.java
+++ app/src/main/java/com/example/jisu/testmapping/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,9 +15,11 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.BroadcastReceiver;
@@ -32,8 +35,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener/*, OnMapReadyCallback */{
     boolean win = false;
+    private LinearLayout ll;
     private static final String TAG = "MainActivity";
     // Log.d(TAG,"");
     LocationManager locManager;
@@ -44,14 +48,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     boolean isLocRequested = false;
     boolean isAlertRegistered = false;
     final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    Button getLocation, alert, alert_release;
+    Button alert, alert_release;
     double lat = 0.0;
     double lng = 0.0;
     GoogleMap gMap;
     GroundOverlayOptions cctvMark;
     ArrayList<MarkerOptions> cctvList = new ArrayList<MarkerOptions>();
-    boolean chcked=false;
-
+    boolean chcked = false;
+    private float min = 200.0f;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,12 +64,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationText = findViewById(R.id.location);
-        getLocation = findViewById(R.id.getLocation);
+        ll = findViewById(R.id.ll);
+
         alert = findViewById(R.id.alert);
         alert_release = findViewById(R.id.alert_release);
         requestRuntimePermission();
-        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
-        getLocation.setOnClickListener(this);
+        try {
+            if (isPermitted) {
+                locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, gpsLocationListener);
+                isLocRequested = true;
+            } else {
+                Toast.makeText(this, "Permission이 없습니다..", Toast.LENGTH_LONG).show();
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+       // ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
         alert.setOnClickListener(this);
         alert_release.setOnClickListener(this);
     }
@@ -100,18 +114,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.getLocation:
-                try {
-                    if (isPermitted) {
-                        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, gpsLocationListener);
-                        isLocRequested = true;
-                    } else {
-                        Toast.makeText(this, "Permission이 없습니다..", Toast.LENGTH_LONG).show();
-                    }
-                } catch (SecurityException e) {
-                    e.printStackTrace();
-                }
-                break;
             case R.id.alert:
                 receiver = new AlertReceiver();
                 IntentFilter filter = new IntentFilter("com.example.jisu.testmapping");
@@ -120,7 +122,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 proximityIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
                 try {
                     win = true;
-                    locManager.addProximityAlert(37.562230, 127.035159, 5.0f, -1, proximityIntent);
+                    locManager.addProximityAlert(37.562019, 127.035559, min, -1, proximityIntent);
                 } catch (SecurityException e) {
                     e.printStackTrace();
                 }
@@ -128,6 +130,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 locationText.setText("위도 : " + lat + "\n" + "경도 : " + lng);
                 break;
             case R.id.alert_release:
+                min=200.0f;
+                ll.setBackgroundColor(Color.BLACK);
                 try {
                     if (isAlertRegistered) {
                         locManager.removeProximityAlert(proximityIntent);
@@ -161,19 +165,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     final LocationListener gpsLocationListener = new LocationListener() {
+        //단점은 움직여서 값이 변동이 되야 한다 그래야 작동한다.
         public void onLocationChanged(Location location) {
             lat = location.getLongitude();
             lng = location.getLatitude();
             locationText.setText("위도 : " + lat + "\n" + "경도 : " + lng);
             if (win) {
                 try {
-                    locManager.addProximityAlert(37.560815, 127.034740, 5.0f, -1, proximityIntent);
+                    locManager.addProximityAlert(37.562019, 127.035559, min, -1, proximityIntent);
                 } catch (SecurityException e) {
                     e.printStackTrace();
                 }
             }
-            chcked=true;
-            ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(MainActivity.this);
+//            chcked = true;
+//            ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(MainActivity.this);
         }
 
         public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -193,34 +198,76 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             boolean isEntering = intent.getBooleanExtra(LocationManager.KEY_PROXIMITY_ENTERING, false);
             Log.d(TAG, "AlertReceiver" + isEntering);
             if (isEntering) {
-                Toast.makeText(context, "목표 지점에 접근중입니다..", Toast.LENGTH_LONG).show();
+                int mm = (int) min;
+                switch (mm) {
+                    case 20:
+                        Toast.makeText(context, "목표에 도착했습니다.", Toast.LENGTH_LONG).show();
+                        ll.setBackgroundColor(Color.YELLOW);
+                        Log.d(TAG,"4번");
+                        break;
+                    case 50:
+                        Toast.makeText(context, "목표반경 50미터 안에 들어왔습니다.", Toast.LENGTH_LONG).show();
+                        ll.setBackgroundColor(Color.BLUE);
+                        min = 20.0f;
+                        Log.d(TAG,"3번");
+                        break;
+                    case 100:
+                        Toast.makeText(context, "목표반경 100미터 안에 들어왔습니다.", Toast.LENGTH_LONG).show();
+                        min = 50.0f;
+                        Log.d(TAG,"2번");
+                        ll.setBackgroundColor(Color.RED);
+                        break;
+                    case 200:
+                        Toast.makeText(context, "목표반경 200미터 안에 들어왔습니다.", Toast.LENGTH_LONG).show();
+                        ll.setBackgroundColor(Color.GREEN);
+                        Log.d(TAG,"1번");
+                        min = 100.0f;
+                        break;
+                }
             } else {
-                Toast.makeText(context, "목표 지점에서 벗어납니다..", Toast.LENGTH_LONG).show();
+                int mm = (int) min;
+                switch (mm) {
+                    case 20:
+                        Toast.makeText(context, "목표반경 20미터 멀어졌습니다.", Toast.LENGTH_LONG).show();
+                        min = 50.0f;
+                        break;
+                    case 50:
+                        Toast.makeText(context, "목표반경 50미터 멀어졌습니다.", Toast.LENGTH_LONG).show();
+                        min = 100.0f;
+                        break;
+                    case 100:
+                        Toast.makeText(context, "목표반경 100미터 멀어졌습니다.", Toast.LENGTH_LONG).show();
+                        min = 200.0f;
+                        break;
+                    case 200:
+                        Toast.makeText(context, "목표에서 200미터 멀어졌습니다", Toast.LENGTH_LONG).show();
+                        break;
+                }
             }
         }
     }
-
-    @Override
+    //맵
+    /*@Override
     public void onMapReady(final GoogleMap googleMap) {
         gMap = googleMap;
         gMap.clear();
-        if(chcked){
-            LatLng latLng=new LatLng(lng,lat);
-            if(cctvList.size()<=0){
-                MarkerOptions marker=new MarkerOptions();
-                marker.position(new LatLng(37.562230, 127.035159));
+        if (chcked) {
+            LatLng latLng = new LatLng(lng, lat);
+            if (cctvList.size() <= 0) {
+                MarkerOptions marker = new MarkerOptions();
+                marker.position(new LatLng(37.561698, 127.034293));
                 cctvList.add(marker);
             }
-            MarkerOptions markerOptions=new MarkerOptions();
+            MarkerOptions markerOptions = new MarkerOptions();
             markerOptions.title("내 위치");
             markerOptions.position(latLng);
             cctvList.add(markerOptions);
             for (int i = 0; i < cctvList.size(); i++) {
                 gMap.addMarker(cctvList.get(i));
             }
-            if(cctvList.size()>=2){
+            if (cctvList.size() >= 2) {
                 cctvList.remove(1);
             }
         }
-    }
+    }*/
 }
